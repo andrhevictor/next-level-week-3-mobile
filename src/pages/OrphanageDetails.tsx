47c81c262f22d7d530e7c148f5e0400a5dec56b7
Andrhé Victor <andrhevictor@gmail.com>
2020-10-16 19:03:31 -0400

    Routes

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
+import { Feather } from "@expo/vector-icons";
+
+import mapMarker from "../images/map-marker.png";
+
+export default function OrphanageDetails() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
